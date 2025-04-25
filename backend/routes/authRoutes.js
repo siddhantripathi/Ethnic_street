86df27d689c2517cd81db8957077b8058dd26901
Siddhant Tripathi <siddhant2000k@gmail.com>
2025-04-24 18:45:47 -0700

    added some stuff

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -67,7 +67,7 @@ router.post('/login', async (req, res) => {
 // Test route to verify user collection
 router.get('/users/test', async (req, res) => {
   try {
-    const users = await User.find({}).collection('users');
+    const users = await User.find({});
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching users' });
